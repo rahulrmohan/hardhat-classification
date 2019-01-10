29cb5ad731f588acd01a55df43fb5df73f6d5b7b
Rahul USC <rahulusc@usc-guestwireless-upc-newsc18669.usc.edu>
2019-01-10 13:26:32 -0800

    Added finished training code

--- train_model.py
+++ train_model.py
@@ -1,22 +1,30 @@
-import cv2
-import glob
 import numpy as np
+np.random.seed(7)
+from tensorflow import set_random_seed
+set_random_seed(2)
+import scipy.misc
+import glob
 import random
 from sklearn.model_selection import train_test_split
 from keras.applications.resnet50 import preprocess_input
 from keras.applications.resnet50 import ResNet50
+from keras.applications.inception_v3 import InceptionV3
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint, EarlyStopping
-from keras.models import Model
+from keras.models import Model, Sequential
+from keras.layers.core import Activation
 from keras import optimizers as opt
 from keras.layers import *
+import os
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
 def setup_to_finetune(model, n):
     # Setting everything bellow n to be not trainable
     for i, layer in enumerate(model.layers):
         layer.trainable = i > n
     model.compile(
-        optimizer=opt.SGD(lr=0.001),
+        optimizer="adam",
         loss='categorical_crossentropy',
         metrics=['accuracy']
     )
@@ -35,16 +43,13 @@ def load_data():
 	all_imgs = np.zeros((2160, 299, 299, 3))
 	all_labels = np.zeros((2160, 3))
 	for i in range(len(hardhat_imgs)):
-		print(i)
-		all_imgs[i, :, :, :] = preprocess_input(cv2.imread(hardhat_imgs[i]))
+		all_imgs[i, :, :, :] = preprocess_input(scipy.misc.imread(hardhat_imgs[i]))
 		all_labels[i, 0] = 1
 	for i in range(len(people_imgs)):
-		print(i)
-		all_imgs[i, :, :, :] = preprocess_input(cv2.imread(people_imgs[i]))
+		all_imgs[i, :, :, :] = preprocess_input(scipy.misc.imread(people_imgs[i]))
 		all_labels[i, 1] = 1
 	for i in range(len(crane_imgs)):
-		print(i)
-		all_imgs[i, :, :, :] = preprocess_input(cv2.imread(crane_imgs[i]))
+		all_imgs[i, :, :, :] = preprocess_input(scipy.misc.imread(crane_imgs[i]))
 		all_labels[i, 2] = 1
 	all_imgs, all_labels = unison_shuffled(all_imgs, all_labels)
 	idxs = list(range(len(all_labels)))
@@ -57,12 +62,41 @@ def load_data():
 	y_test = all_labels[test_idxs, :]
 	return X_train, X_test, y_train, y_test
 
+def train_simple_cnn(X_train, X_test, y_train, y_test):
+	batch_size = 32
+	steps_per_epoch = len(X_train) / batch_size
+	num_classes = 3
+	train_gen = ImageDataGenerator(
+	    rotation_range=30,
+	    width_shift_range=0.2,
+	    height_shift_range=0.2,
+	    shear_range=0.2,
+	    zoom_range=0.2,
+	    horizontal_flip=True
+	)
+	model = Sequential()
+	model.add(Conv2D(128, (2,2), input_shape = (299, 299, 3), activation = 'relu'))
+	model.add(MaxPooling2D(pool_size = (2, 2)))
+	model.add(Conv2D(64, (3,3), activation = 'relu'))
+	model.add(MaxPooling2D(pool_size = (2, 2)))
+	model.add(Conv2D(32, (2,2), activation = 'relu'))
+	model.add(MaxPooling2D(pool_size = (2, 2)))
+	model.add(Flatten())
+	model.add(Dense(units = 128, activation = 'relu'))
+	model.add(Dropout(0.3))
+	model.add(Dense(units = 10, activation = 'relu'))
+	model.add(Dropout(0.3))
+	model.add(Dense(units = 3, activation = 'softmax'))
+	model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+	model.fit_generator(train_gen.flow(X_train, y_train, batch_size=batch_size), steps_per_epoch=steps_per_epoch, epochs=10, validation_data=(X_test, y_test))
+	model.save('hardhat_detector.h5')
+
 def train_resnet50(X_train, X_test, y_train, y_test):
 	print("compiling model...")
 	batch_size = 32
 	steps_per_epoch = len(X_train) / batch_size
 	num_classes = 3
-	base_model = ResNet50(weights="imagenet", include_top=False)
+	base_model = InceptionV3(weights="imagenet", include_top=False)
 	last = base_model.output
 	last = GlobalAveragePooling2D()(last)
 	pred = Dense(num_classes, activation='softmax')(last)
@@ -81,4 +115,4 @@ def train_resnet50(X_train, X_test, y_train, y_test):
 
 if __name__ == '__main__':
 	X_train, X_test, y_train, y_test = load_data()
-	train_resnet50(X_train, X_test, y_train, y_test)
+	train_simple_cnn(X_train, X_test, y_train, y_test)
